049867be3b6aed918cb5712116a64111f307cf84
Vageesh Pandith <137815077+vageeshpandith@users.noreply.github.com>
2024-02-18 22:41:49 +0530

    Multiple Files of Structure

---
+++ C_Programs/structure/emp.h
@@ -0,0 +1,7 @@
+typedef struct emp
+{
+	int eid;
+	float sal;
+	char name[20];
+}emp;
+emp showemp(*);
---
+++ C_Programs/structure/fundef.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+#include "emp.h"
+
+int main()
+{
+	emp e1,e2;
+	e1.eid=12342;
+	strcpy(e1.name,"Vagesh ");
+	e1.sal=95000;
+	
+	e2.eid=24236;
+	e2.sal=85000;
+	strcpy(e2.name,"Emin ");
+	showemp(&e1);
+	showemp(&e2);
+}
+void showemp(emp *ptr)
+{
+	printf("Name=%s\n",ptr->name);
+	printf("Eid=%d\n",ptr->eid);
+	printf("Salary=%f\n",ptr->sal);
+}
